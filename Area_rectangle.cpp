ee37b7ae5eb658689175f7a79369aabd0eb71a41
snehasajan-code <snehasajan1508@gmail.com>
2025-08-30 00:07:32 +0530

    Add files via upload

---
+++ Area_rectangle.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    /*declaring three variables */
+    float length,width,area;
+
+    /*input length and width of a rectangle*/
+    cout<<"Enter the length: ";
+    cin>>length;
+    cout<<"Enter the width: ";
+    cin>>width;
+
+    /*applying area of rectangle formula */
+    area=length*width;
+
+    /*printing the area*/
+    cout<<"Area of a rectangle is: "<<area;
+    return 0;
+
+
+
+
+}
