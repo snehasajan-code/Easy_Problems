a06f193aedb0687e4579a36051656cf550d3d288
snehasajan-code <snehasajan1508@gmail.com>
2025-08-30 00:06:55 +0530

    Add files via upload

---
+++ Sum.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    /*declaring three variables*/
+    int number_1,number_2,sum;
+
+    /*input 2 numbers*/
+    cout<<"Enter first number:";
+    cin>>number_1;
+    cout<<"Enter second number:";
+    cin>>number_2;
+
+    /*adding and storing the two numbers */
+    sum=number_1+number_2;
+
+    /*printing the sum*/
+    cout<<"Sum is: "<<sum;
+    return 0;
+
+
+
+
+}
