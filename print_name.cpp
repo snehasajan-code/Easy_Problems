8a2d807202fd4079a622b3ca381962896c73c63d
snehasajan-code <snehasajan1508@gmail.com>
2025-08-30 00:06:05 +0530

    Add files via upload

---
+++ print_name.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    /*declaring a variable */
+    string name;
+
+    /*input a name*/
+    cout<<"Enter a name: ";
+    cin>>name;
+
+    /*printing the output*/
+    cout<<"Namah Shivaya, "<<name;
+    return 0;
+
+
+
+
+}
