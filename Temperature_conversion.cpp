a8cf3ab1b54b223481ceb22789e70e0092afb587
snehasajan-code <snehasajan1508@gmail.com>
2025-08-30 00:08:12 +0530

    Add files via upload

---
+++ Temperature_conversion.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    /*declaring two variables */
+    float celsius,fahrenheit;
+
+    /*input degree in celsius*/
+    cout<<"Enter the celsius: ";
+    cin>>celsius;
+
+    /*applying conversion of degree celsius into fahrenheit formula */
+    fahrenheit=(celsius*9/5)+32;
+
+    /*printing the fahrenheit*/
+    cout<<"Degree in fahrenheit: "<<fahrenheit;
+    return 0;
+
+
+
+
+}
