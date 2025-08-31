1b95fd48f46817cc07f0829f9be9ff9d094aae55
snehasajan-code <snehasajan1508@gmail.com>
2025-08-31 11:54:01 +0530

    Add files via upload

---
+++ Square.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    /*declaring two variables */
+    int number_1,square;
+
+    /*input a number*/
+    cout<<"Enter a number:";
+    cin>>number_1;
+
+    /*calculating square of a number */
+    square=number_1*number_1;
+    cout<<"Square of "<<number_1<<" is: "<<square;
+
+    return 0;
+
+}
