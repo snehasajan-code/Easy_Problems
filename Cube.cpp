389fd9de5ae044975ffb3ddf27a243ff1cd52e4a
snehasajan-code <snehasajan1508@gmail.com>
2025-08-31 11:54:15 +0530

    Add files via upload

---
+++ Cube.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    /*declaring two variables */
+    int number_1,cube;
+
+    /*input a number*/
+    cout<<"Enter a number:";
+    cin>>number_1;
+
+    /*calculating cube of a number */
+    cube=number_1*number_1*number_1;
+    cout<<"Cube of "<<number_1<<" is: "<<cube;
+
+    return 0;
+
+}
