eabdb313a97bff041dd3320a622691da4a8b0f4d
snehasajan-code <snehasajan1508@gmail.com>
2025-08-31 11:56:12 +0530

    Add files via upload

---
+++ Natural_numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+
+    cout << "The first 10 natural numbers are: ";
+    /* Using loop */
+    for(i=1;i<=10;i++)
+    {
+        cout<<i<<" ";
+    }
+
+    return 0;
+}
